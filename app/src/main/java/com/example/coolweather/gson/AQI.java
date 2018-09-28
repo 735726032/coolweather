ebe2410fa0cdb491f07a667f7b501e1b0adf26c0
gu <735726032@qq.com>
2018-09-28 21:23:28 +0800

    完成遍历省市县三级列表与对天气返回数据封装成gson实体类

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,11 @@
+package com.example.coolweather.gson;
+
+public class AQI {
+
+    public AQIcity aqi;
+
+    public class AQIcity {
+        public String aqi;
+        public String pm25;
+    }
+}
